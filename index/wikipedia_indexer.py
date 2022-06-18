a5f9ff68c67e45e374147a833d2d96353a2bce4f
Davi Emediato <48991554+daviemediato@users.noreply.github.com>
2022-06-18 17:41:47 -0300

    activity 16 fix

--- index/wikipedia_indexer.py
+++ index/wikipedia_indexer.py
@@ -1,10 +1,13 @@
-from index.indexer import *
-from index.structure import *
+from indexer import *
+from structure import *
 
 if __name__ == "__main__":
     HTMLIndexer.cleaner = Cleaner(stop_words_file="stopwords.txt",
-                        language="portuguese",
-                        perform_stop_words_removal=True,
-                        perform_accents_removal=True,
-                        perform_stemming=False)
-    
+                                  language="portuguese",
+                                  perform_stop_words_removal=True,
+                                  perform_accents_removal=True,
+                                  perform_stemming=False)
+
+    obj_index = FileIndex()
+    html_indexer = HTMLIndexer(obj_index)
+    html_indexer.index_text_dir("")
